50ef7308a6174a95af39924732e7c6a4535817bd
Felipe Rohde <feliperohdee@gmail.com>
2017-04-29 00:43:12 -0300

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const stringifyStack = x => {
+	return JSON.stringify(x)
+		.replace(/\"/g, '')
+		.replace(/\t/g, '')
+		.replace(/\\t/g, '')
+		.replace(/\s{2,}/g, '')
+		.replace(/\\s{2,}/g, '')
+		.replace(/\\n/g, '\n')
+		.split('\n')
+		.slice(1);
+};
+
+const beautify = (err, context = {}) => {
+	if(!(err instanceof Error)){
+		err = new Error(err);
+	}
+
+	return {
+		name: err.name,
+		message: err.message,
+		code: err.code || err.statucCode || 500,
+		context,
+		stack: stringifyStack(err.stack)
+	};
+};
+
+module.exports = beautify;
